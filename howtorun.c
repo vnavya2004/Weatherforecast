7158bf34c8c0aa78fe30e38ff4a873fe01936f51
vnavya2004 <118450993+vnavya2004@users.noreply.github.com>
2023-08-19 15:16:48 +0530

    Add files via upload

---
+++ howtorun.c
@@ -0,0 +1,9 @@
+//make internet on bcs api is taken from OpenWeather app so to work that we need it.
+//pc->newvolume->weatherproject
+//run all these in terminal 
+//make sure environment (djangoenv) in green is live if not make it active using djangoenv\Scripts\activate something like this
+//int the directory everything should run because manage.py is inthis "D:\weatherproject\weatherproject>"
+//run the command as"python manage.py runserver"
+//you will get a http link go to that and proceed
+//if you give wrong city it will be crashed so dont give that.
+ 
